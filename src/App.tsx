20fa01b8747fe914ea7d5c0fd496c7de99a217c8
Tugcanyilmaz <142771887+Tugcanyilmaz@users.noreply.github.com>
2025-11-06 01:42:29 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,22 @@
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Statistics from './components/Statistics';
+import Testimonials from './components/Testimonials';
+import ContactForm from './components/ContactForm';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Hero />
+      <Features />
+      <Statistics />
+      <Testimonials />
+      <ContactForm />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
 
